3fcbcd5d9987473faeb0b829dbcb010d64642344
Emre Hızlı <hizliemre@gmail.com>
2022-09-24 02:15:58 +0300

    f

--- src/app/custom/+state/todo.state.ts
+++ src/app/custom/+state/todo.state.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Guid } from 'guid-typescript';
 import { merge, Observable, Subject } from 'rxjs';
-import { map, shareReplay, startWith } from 'rxjs/operators';
-import { TodoItem } from 'src/app/shared/todo-list/todo-item/todo-item.component';
+import { map, scan, shareReplay, startWith } from 'rxjs/operators';
+import { TodoItem, TodoItemEvent } from 'src/app/shared/todo-list/todo-item/todo-item.component';
 
 interface Action {
   type: string;
@@ -45,6 +45,45 @@ export class TodoStore {
   state$: Observable<TodoState> =
     this.dispatcher$.pipe(
       startWith(defaultState as any),
+      scan((state: TodoState, action: Action): TodoState => {
+        switch (action.type) {
+          case 'add':
+            {
+              let newState = { ...state };
+              newState = {
+                ...newState,
+                items: [...newState.items, action.payload]
+              }
+              return newState;
+            }
+          case 'update':
+            {
+              let newState = { ...state };
+              let updatedItems = newState.items.reduce((prev, curr) => ({ ...prev, [curr.id]: curr }), {} as any);
+              updatedItems = {
+                ...updatedItems,
+                [action.payload.id]: { ...updatedItems[action.payload.id], done: action.payload.done }
+              };
+              newState = {
+                ...newState,
+                items: Object.values(updatedItems)
+              }
+              return newState;
+            }
+          case 'remove': {
+            const newStateItems = [...state.items];
+            let index = state.items.findIndex((m) => m.id === action.payload);
+            newStateItems.splice(index, 1);
+            const newState = {
+              ...state,
+              items: newStateItems
+            }
+            return newState;
+          }
+          default:
+            return state;
+        }
+      }, defaultState),
       shareReplay({ refCount: true, bufferSize: 1 })
     );
 
@@ -53,5 +92,16 @@ export class TodoStore {
    */
   items$: Observable<TodoItem[]> =
     this.state$.pipe(map((state) => state.items));
+
+  /**
+  * Actions
+  */
+  public add(item: TodoItem) { this.dispatch({ type: 'add', payload: item }); }
+  public update($event: TodoItemEvent) { this.dispatch({ type: 'update', payload: $event }); }
+  public remove(id: string) { this.dispatch({ type: 'remove', payload: id }); }
+
+  private dispatch(action: Action): void {
+    this.actions$.next(action);
+  }
 
 }
