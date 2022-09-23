0313d9f94468d755fbd2e3a70f3bcbee00b9ac86
Emre Hızlı <hizliemre@gmail.com>
2022-09-23 15:53:25 +0300

    f

---
+++ src/app/ngrx/+state/facade.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { TodoItem, TodoItemEvent } from 'src/app/shared/todo-list/todo-item/todo-item.component';
+import { todoListActions } from './actions';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoStateFacade {
+
+  constructor(private readonly _store: Store) { }
+
+  public update($event: TodoItemEvent): void {
+    this._store.dispatch(todoListActions.update($event));
+  }
+
+  public remove(id: string): void {
+    this._store.dispatch(todoListActions.remove({ id }));
+  }
+
+  public add(item: TodoItem): void {
+    this._store.dispatch(todoListActions.add(item));
+  }
+
+}
