10199282bcd9a6d52e2db421bf18cc20f2ca28e4
Emre Hızlı <hizliemre@gmail.com>
2022-09-23 13:27:23 +0300

    f

--- src/app/ng-service/+state/todo-state.service.ts
+++ src/app/ng-service/+state/todo-state.service.ts
@@ -1,17 +1,13 @@
 import { Injectable } from '@angular/core';
-import { Guid } from 'guid-typescript';
-import { BehaviorSubject, map } from 'rxjs';
-import { TodoItem, TodoItemEvent } from 'src/app/shared/todo-list/todo-item/todo-item.component';
+import { BehaviorSubject } from 'rxjs';
+import { TodoItem } from 'src/app/shared/todo-list/todo-item/todo-item.component';
 
 interface NgServiceState {
   items: TodoItem[];
 }
 
 const initialValue: NgServiceState = {
-  items: [
-    { id: Guid.create().toString(), done: true, title: 'Lorem ipsum dolor sit amet' },
-    { id: Guid.create().toString(), done: true, title: 'Lorem ipsum dolor sit amet' },
-    { id: Guid.create().toString(), done: true, title: 'Lorem ipsum dolor sit amet' }]
+  items: []
 }
 
 @Injectable({
@@ -21,16 +17,4 @@ export class TodoStateService {
 
   private readonly _store$ = new BehaviorSubject<NgServiceState>(initialValue);
 
-  public items$ = this._store$.pipe(map((state) => state.items));
-
-  public update($event: TodoItemEvent): void {
-    const currentState = this._store$.value;
-    const updatedItems = currentState.items.reduce((prev, curr) => ({ ...prev, [curr.id]: { ...curr, ...$event } }), {});
-    const newState = {
-      ...currentState,
-      items: Object.values(updatedItems) as TodoItem[]
-    }
-    console.log(newState);
-    this._store$.next(newState);
-  }
 }
