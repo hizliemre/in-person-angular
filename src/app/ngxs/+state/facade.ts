ae9527e2d8fe337ea82a68a49c92a97fce35733b
Emre Hızlı <hizliemre@gmail.com>
2022-09-23 20:33:05 +0300

    f

--- src/app/ngxs/+state/facade.ts
+++ src/app/ngxs/+state/facade.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Select } from '@ngxs/store';
+import { Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { TodoItem } from 'src/app/shared/todo-list/todo-item/todo-item.component';
+import { TodoListActions } from './actions';
 import { featureKey } from './state';
 
 @Injectable({
@@ -10,5 +11,11 @@ import { featureKey } from './state';
 export class NgxsFacadeService {
 
   @Select((state: any) => state[featureKey].items) items$!: Observable<TodoItem[]>;
+
+  constructor(private readonly _store: Store) { }
+
+  public add(item: TodoItem): void {
+    this._store.dispatch(new TodoListActions.Add(item))
+  }
 
 }
