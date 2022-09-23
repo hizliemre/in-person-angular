b85ed2908aa81ed0ccbe0ab6d575d87b30b8b2be
Emre Hızlı <hizliemre@gmail.com>
2022-09-24 01:59:57 +0300

    f

--- src/app/custom/+state/todo.state.ts
+++ src/app/custom/+state/todo.state.ts
@@ -1,4 +1,19 @@
+import { Guid } from 'guid-typescript';
+import { TodoItem } from 'src/app/shared/todo-list/todo-item/todo-item.component';
+
 interface Action {
   type: string;
   payload?: any;
 }
+
+interface TodoState {
+  items: TodoItem[];
+}
+
+const defaultState: TodoState = {
+  items: [
+    { id: Guid.create().toString(), done: true, title: 'Lorem ipsum dolor sit amet' },
+    { id: Guid.create().toString(), done: true, title: 'Lorem ipsum dolor sit amet' },
+    { id: Guid.create().toString(), done: true, title: 'Lorem ipsum dolor sit amet' }
+  ],
+};
