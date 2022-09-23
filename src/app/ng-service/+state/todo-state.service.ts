5fe4c60bf2810a260d2fd5d82fc50796b7cee8f8
Emre Hızlı <hizliemre@gmail.com>
2022-09-23 13:11:00 +0300

    ng-service1

--- src/app/ng-service/+state/todo-state.service.ts
+++ src/app/ng-service/+state/todo-state.service.ts
@@ -1,6 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Guid } from 'guid-typescript';
+import { BehaviorSubject, map } from 'rxjs';
+import { TodoItem } from 'src/app/shared/todo-list/todo-item/todo-item.component';
+
+interface NgServiceState {
+  items: TodoItem[];
+}
+
+const initialValue: NgServiceState = {
+  items: [
+    { id: Guid.create().toString(), done: true, title: 'Lorem ipsum dolor sit amet' },
+    { id: Guid.create().toString(), done: true, title: 'Lorem ipsum dolor sit amet' },
+    { id: Guid.create().toString(), done: true, title: 'Lorem ipsum dolor sit amet' }]
+}
 
 @Injectable({
   providedIn: 'root'
 })
-export class TodoStateService { }
+export class TodoStateService {
+
+  private readonly _store$ = new BehaviorSubject<NgServiceState>(initialValue);
+
+  public items$ = this._store$.pipe(map((state) => state.items));
+}
--- src/app/ng-service/ng-service.component.ts
+++ src/app/ng-service/ng-service.component.ts
@@ -1,6 +1,5 @@
+import { CommonModule } from '@angular/common';
 import { Component, HostBinding } from '@angular/core';
-import { Guid } from 'guid-typescript';
-import { TodoItem } from '../shared/todo-list/todo-item/todo-item.component';
 import { TodoListComponent } from '../shared/todo-list/todo-list.component';
 import { TodoStateService } from './+state/todo-state.service';
 
@@ -8,16 +7,11 @@ import { TodoStateService } from './+state/todo-state.service';
   selector: 'app-ng-service',
   templateUrl: './ng-service.component.html',
   standalone: true,
-  imports: [TodoListComponent]
+  imports: [CommonModule, TodoListComponent]
 })
 export class NgServiceComponent {
 
   @HostBinding('class') classList = 'flex justify-center p-16 items-start w-full h-full'
-
-  items: TodoItem[] = [
-    { id: Guid.create().toString(), done: true, title: 'Lorem ipsum dolor sit amet' },
-    { id: Guid.create().toString(), done: true, title: 'Lorem ipsum dolor sit amet' },
-    { id: Guid.create().toString(), done: true, title: 'Lorem ipsum dolor sit amet' }]
 
   constructor(public readonly state: TodoStateService) { }
 
