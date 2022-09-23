cd362c805367c7533925236739efe25a75509462
Emre Hızlı <hizliemre@gmail.com>
2022-09-23 16:25:37 +0300

    f

--- src/app/ngrx/+state/reducer.ts
+++ src/app/ngrx/+state/reducer.ts
@@ -17,7 +17,22 @@ const initialValue: NgrxState = {
 
 export const reducer = createReducer(
   initialValue,
-  on(todoListActions.add, (state) => { return state; }),
-  on(todoListActions.add, (state) => { return state; }),
-  on(todoListActions.add, (state) => { return state; }),
+  on(todoListActions.add, (state, action) => {
+    const newState = { ...state };
+    newState.items.push(action.item);
+    return newState;
+  }),
+  on(todoListActions.update, (state, action) => {
+    const newState = { ...state };
+    let updated = newState.items.find((m) => m.id === action.event.id);
+    if (!updated) return state;
+    updated.done = action.event.done;
+    return newState;
+  }),
+  on(todoListActions.remove, (state, action) => {
+    const newState = { ...state };
+    let index = newState.items.findIndex((m) => m.id === action.id);
+    newState.items.splice(index, 1);
+    return state;
+  }),
 )
