9d9af004268477669d1dbaae6cc78f4604e81b66
Emre Hızlı <hizliemre@gmail.com>
2022-10-26 18:00:56 +0300

    a

--- src/app/akita/+state/todo.store.ts
+++ src/app/akita/+state/todo.store.ts
@@ -1,11 +1,21 @@
 import { EntityState, EntityStore, StoreConfig } from '@datorama/akita';
+import { Guid } from 'guid-typescript';
 import { TodoItem } from 'src/app/shared/todo-list/todo-item/todo-item.component';
 
 export interface TodosState extends EntityState<TodoItem, string> { }
 
+const initialEntities: TodoItem[] = [
+  { id: Guid.create().toString(), done: true, title: 'Lorem ipsum dolor sit amet' },
+  { id: Guid.create().toString(), done: true, title: 'Lorem ipsum dolor sit amet' },
+  { id: Guid.create().toString(), done: true, title: 'Lorem ipsum dolor sit amet' }]
+
+const initialState: TodosState = {
+  entities: initialEntities.reduce((acc, item) => ({ ...acc, [item.id]: item }), {}),
+  ids: initialEntities.map(item => item.id)
+}
 @StoreConfig({ name: 'todos' })
 export class TodosStore extends EntityStore<TodosState> {
   constructor() {
-    super();
+    super(initialState);
   }
 }
